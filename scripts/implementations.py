438b87f1f0dfcf2c6fb88c89978961e7bdf361cc
walterieux <david.desboeufs@hotmail.fr>
2020-09-28 16:48:32 +0200

    submission.csv

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -7,6 +7,7 @@ Created on Thu Sep 24 15:47:51 2020
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 def compute_loss(y, tx, w):
     """Calculate the loss.
     You can calculate the loss using mse or mae.
@@ -46,9 +47,9 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     n = len(y)
     print("max_iters = " + repr(max_iters))
     w = initial_w
-    w_toplot=[]
+    w_toplot = []
     w_toplot.append(w)
-    
+
     for n_iter in range(max_iters):
 
         # 1) on pourrait rajouter une autre condition ici assez simple qui regarde l'écart entre deux différents w et si jamais 
@@ -58,34 +59,32 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
         current_gradient = compute_gradient(y, tx, w, n)
         if np.linalg.norm(current_gradient) <= 1e-6:
             break
-        w = w - gamma/(n_iter+1) * compute_gradient(y, tx, w, n)
+        w = w - gamma / (n_iter + 1) * compute_gradient(y, tx, w, n)
         w_toplot.append(w)
-    
-    
-    toplot= np.zeros((max_iters,1))
+
+    toplot = np.zeros((max_iters, 1))
     for i in range(max_iters):
-        toplot[i]=compute_loss(y, tx,w_toplot[i] )
+        toplot[i] = compute_loss(y, tx, w_toplot[i])
     print(toplot)
-    plt.plot(np.linspace(1,max_iters,max_iters),toplot)
-    plt.scatter(np.linspace(1,max_iters,max_iters),toplot)
+    plt.plot(np.linspace(1, max_iters, max_iters), toplot)
+    plt.scatter(np.linspace(1, max_iters, max_iters), toplot)
     plt.title("Error in term of number of iterations")
     plt.xlabel("number of iterations")
     plt.ylabel("Error")
     plt.yscale("log")
-    
-    
+
     return w, compute_loss(y, tx, w)
 
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     n = len(y)
 
-    rnd_sample = np.random.random_integers(0, n, max_iters)
+    rnd_sample = np.random.random_integers(0, n - 1, max_iters)
 
     w = initial_w
     for n_iter in range(max_iters):
         subgradient = compute_gradient(y[rnd_sample[n_iter]], tx[rnd_sample[n_iter], :], w, n)
         if np.linalg.norm(subgradient) <= 1e-6:
             break
-        w = w - gamma/(n_iter+1) * subgradient
+        w = w - gamma / (n_iter + 1) * subgradient
     return w, compute_loss(y, tx, w)
