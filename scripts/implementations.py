7387777c693a9f7727d1517c08445bced1495d32
Jean Gillain <jeangillain@homtail.com>
2020-10-25 16:54:54 +0100

    ajout des figures + sans standardiser

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -69,7 +69,6 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma, toplot=False):
 
     # Define parameters to store w and loss
     w = initial_w
-    w = sanitize_w_shape(w)
     gradient_to_plot = []
 
     for n_iter in range(max_iters):
@@ -92,7 +91,6 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma, toplot=False):
     rnd_sample = np.random.random_integers(0, n - 1, max_iters)
     gradient_to_plot = []
     w = initial_w
-    w = sanitize_w_shape(w)
 
     for n_iter in range(max_iters):
         subgradient = compute_gradient(y[rnd_sample[n_iter]], tx[rnd_sample[n_iter], :], w)
@@ -133,7 +131,6 @@ def logistic_regression_S(y, tx, initial_w, max_iters, gamma, toplot=False):
     """Logistic regression using stochastic gradient descent"""
 
     w = initial_w
-    w = sanitize_w_shape(w)
     gradient_to_plot = []
 
     rnd_sample = np.random.random_integers(0, len(y) - 1, max_iters)
@@ -155,7 +152,6 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma: float, toplot=False)
     """Logistic regression using gradient descent"""
 
     w = initial_w
-    w = sanitize_w_shape(w)
     gradient_to_plot = []
 
     for n_iter in range(max_iters):
@@ -174,7 +170,6 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     """Regularized logistic regression using gradient descent"""
 
     w = initial_w
-    w = sanitize_w_shape(w)
 
     for n_iter in range(max_iters):
         s = sigma(np.dot(tx, w) - y)
@@ -190,7 +185,6 @@ def newton_logistic_regression_s(y, tx, initial_w, max_iters, gamma, toplot=Fals
     """Logistic regression using newton's method with stochastic gradient descent"""
 
     w = initial_w
-    w = sanitize_w_shape(w)
     gradient_to_plot = []
 
     rnd_sample = np.random.random_integers(0, len(y) - 1, max_iters)
@@ -215,7 +209,6 @@ def newton_logistic_regression(y, tx, initial_w, max_iters, gamma):
     """Logistic regression using newton's method with gradient descent"""
 
     w = initial_w
-    w = sanitize_w_shape(w)
 
     for n_iter in range(max_iters):
         s_sigma = sigma(np.dot(tx, w))
